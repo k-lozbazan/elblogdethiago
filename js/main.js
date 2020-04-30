1245b746c4c3e46e707ab0079ee258b860531a5e
k-loz <solucionesti.pwdkloz@gmail.com>
2020-04-30 18:30:12 -0500

    Modificacion de Cards Mis Padres

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function() {
+
+    //Lettering
+    $('.nombre-sitio').lettering();
+
+    //Menu Fijo
+    var tamañoVentana = $(window).height(); // CUanto mide la ventana
+    var barraAltura = $('.barra').innerHeight(); // Cuanto mide la barra de navegacion
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > tamañoVentana) {
+            $('.barra').addClass('fixed');
+            $('body').css({ 'margin-top': barraAltura + 'px' });
+        } else {
+            $('.barra').removeClass('fixed');
+            $('body').css({ 'margin-top': '0px' });
+        }
+    });
+});
