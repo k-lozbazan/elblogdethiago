bbb9c1d3edaa0c3384e8b2ffd7d18221036e561a
k-loz <solucionesti.pwdkloz@gmail.com>
2020-05-02 18:34:29 -0500

    Galería de imagenes-Carousel

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const elementosCarousel = document.querySelectorAll('.carousel');
     M.Carousel.init(elementosCarousel, {
-        duration: 150
+        duration: 1500
     });
 });
 
